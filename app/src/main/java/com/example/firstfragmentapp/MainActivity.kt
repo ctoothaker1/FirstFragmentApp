06651ae14538f3c60431623d5149a3356da7796d
ctoothaker1 <ctoothaker1@gmail.com>
2024-10-10 12:21:00 -0400

    second fragment added and fragment manager added

--- app/src/main/java/com/example/firstfragmentapp/MainActivity.kt
+++ app/src/main/java/com/example/firstfragmentapp/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        // use transaction to add second fragment
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView2, ColorFragment())
+            .commit()
     }
 }
